4876457de7702fda748c10e1853910462bb31904
Kenny Meyer <knny.myer@gmail.com>
2012-08-27 13:12:15 -0400

    Add helper Shortener::ShortenerHelper.full_url

--- app/helpers/shortener/shortener_helper.rb
+++ app/helpers/shortener/shortener_helper.rb
@@ -6,4 +6,14 @@ module Shortener::ShortenerHelper
     short_url ? url_for(:controller => :"shortener/shortened_urls", :action => :show, :id => short_url.unique_key, :only_path => false) : url
   end
 
+  # Get the full URL from a short URL
+  def full_url(short_url)
+    begin
+      short_url = Shortener::ShortenedUrl.where(unique_key: short_url[-5..-1]).first
+    rescue Mongoid::Errors::DocumentNotFound => e
+      nil
+    end
+    return short_url.nil? ? nil : short_url.url
+  end
+
 end
--- spec/helpers/shortener_helper_spec.rb
+++ spec/helpers/shortener_helper_spec.rb
@@ -9,4 +9,15 @@ describe Shortener::ShortenerHelper do
       helper.short_url("www.doorkeeperhq.com").should == "http://test.host/#{@short_url.unique_key}"
     end
   end
+
+  describe "full_url" do
+    it "should return the full URL if the short_url if valid" do
+      helper.full_url("http://test.host/#{@short_url.unique_key}").should == @short_url.url
+    end
+
+    it "should return nil if short_url is invalid" do
+      helper.full_url("http://test.host/blablablabla").should be_nil
+      helper.full_url("http://test.host/").should be_nil
+    end
+  end
 end
